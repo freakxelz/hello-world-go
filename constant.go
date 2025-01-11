a0f74f7a832e173232682ede73e564b00b202507
Freakxelz <devamahendrar@gmail.com>
2025-01-11 07:04:34 +0000

    constant session

---
+++ constant.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	const firstName string = "Asyifa"
+	const middleName string = "Adya"
+	const lastName string = "Maullidya"
+	const age = 24
+
+	fmt.Println(firstName)
+	fmt.Println(middleName)
+	fmt.Println(lastName)
+	fmt.Println( age)
+
+	const(
+
+	fullName string = "Deva Mahendra Ramadhan"
+	)
+
+	fmt.Println(fullName)
+
+
+}
